7526ba7261a5d59abe29ba318d604ae77b474989
Krieldi Mesa Karaeng <kejesks31@gmail.com>
2024-09-04 08:37:29 +0800

    Hari ke tiga

--- Day3.java
+++ Day3.java
@@ -1,9 +1,8 @@
 public class Day3 {
     
     public static void main(String[] args) {
-        String nama = "Krieldi";
         
-        System.out.println("nama:" + nama);
+        System.out.println("Nama: Krieldi");
         System.out.println("NIM: D0224340");
         System.out.println("Kelas: Informatika A");
     }
