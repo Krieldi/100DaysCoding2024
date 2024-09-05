828d3117d6e77c43352b001dc725bcb7ecce2233
Krieldi Mesa Karaeng <kejesks31@gmail.com>
2024-09-05 13:22:20 +0800

    Hari ke empat

---
+++ Day4.java
@@ -0,0 +1,13 @@
+package ISC;
+
+public class day4{
+    public static void main(String[] args) {
+     //variabel 
+     String nim = "D0224340";
+     char kls ='A';
+     //print out
+     System.out.print("NAMA: Krieldi\n");
+     System.out.printf("NIM : %s \n", nim);
+     System.out.printf("kelas : %c \n", kls);
+    }
+}
