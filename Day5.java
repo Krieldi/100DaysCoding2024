41eaeb770ef632f77820c1eff07237b078d2cfc8
Krieldi Mesa Karaeng <kejesks31@gmail.com>
2024-09-06 10:40:26 +0800

    Hari kelima

---
+++ Day5.java
@@ -0,0 +1,19 @@
+package ISC;
+
+public class Day5 {
+    
+    public static void main(String[] args) {
+        // single-line comment
+        //variabel:
+     String nama = "D0224340";
+     char  kls='A';
+        /*multi line comment*/
+        /*untuk print 
+         * dan printf
+         */
+     System.out.print("NAMA: Krieldi\n");
+     System.out.printf("NIM : %s \n", nama);
+     System.out.printf("kelas : %c \n", kls);
+    
+    }
+}
