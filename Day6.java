6d4cd8cf8781cba017d1940cd0c166f3c2d11714
Krieldi Mesa Karaeng <kejesks31@gmail.com>
2024-09-07 09:53:56 +0800

    hari keenam

---
+++ Day6.java
@@ -0,0 +1,16 @@
+package ISC;
+
+public class Day6{
+    
+    public static void main(String[] args) {
+     String nim;
+        nim= "D0224340";
+     char kls;
+        kls = 'A';
+        
+     System.out.print("NAMA: Krieldi\n");
+     System.out.printf("NIM : %s\n", nim);
+     System.out.println("kelas : "+ kls);
+    
+    }
+}
