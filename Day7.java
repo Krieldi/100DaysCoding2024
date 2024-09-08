928de3b4298d9821133702b71b99767ece2c3397
Krieldi Mesa Karaeng <kejesks31@gmail.com>
2024-09-08 18:18:05 +0800

    hari ke tujuh
    Hari ke tujuh ngoding 100 hari

---
+++ Day7.java
@@ -0,0 +1,21 @@
+package ISC;
+
+public class Day7 {
+    
+    public static void main(String[] args) {
+        //variabel inisialisasi
+        String name = "Krieldi";
+        char kls = 'A';
+        String NIM = "D0224340";
+        int age =16;
+        
+        /*
+         * #this is mine
+         */
+        //Here's the print
+        System.out.println("Nama: " + name);
+        System.out.println("Kelas: "+ kls);
+        System.out.print("NIM: "+ NIM);
+        System.out.printf("\nage: %d years old", age);
+    }
+}
