6254c8bace22ef19672e113fa56340b0905b9329
Krieldi Mesa Karaeng <kejesks31@gmail.com>
2024-09-09 09:46:55 +0800

    Hari ke Delapan

---
+++ Day8.java
@@ -0,0 +1,28 @@
+package ISC;
+
+public class Day8 {
+    
+    public static void main(String[] args) {
+        //Variable Update
+        String nama ="slebew ";
+        
+        nama/*Here's the Update*/ = "Krieldi";
+        
+        char cr='z';
+        //Update from character with value "z"
+        cr='A';
+        //well just experiment 
+        int _i= 444;
+        int $= 222;
+        int result= _i + $;
+            //Update the result too 
+        result = _i-$+222;
+        //print its all here
+        System.out.printf("My name is %s\n",nama);
+        System.out.println("I come from informatics class "+ cr);
+            //print my experiment:
+        System.out.println("K"+result);
+        
+        
+    }
+}
