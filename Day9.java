74116cd360a753db048dc662422a921ed30cb640
Krieldi Mesa Karaeng <kejesks31@gmail.com>
2024-09-10 07:54:42 +0800

    Hari ke sembilan

---
+++ Day9.java
@@ -0,0 +1,13 @@
+package ISC;
+
+public class Day9 {
+    
+    public static void main(String[] args) {
+        final int cnth = 123;
+        final String word= "Kata";
+        //final (constanta) tidak akan bisa di ubah/di update
+        System.out.println("Contoh: "+cnth);
+        System.out.println("Contoh2: "+ word);
+    }
+    
+}
