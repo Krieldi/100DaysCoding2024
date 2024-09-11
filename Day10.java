f1de9566d2dacffad40f91a4e208104daa634403
Krieldi Mesa Karaeng <kejesks31@gmail.com>
2024-09-11 18:01:51 +0800

    Hari ke sepuluh

---
+++ Day10.java
@@ -0,0 +1,11 @@
+package ISC;
+
+public class Day10 {
+    
+    public static void main(String[] args) {
+        byte bit= -128;//angkanya dari -128 sampai 127
+          
+        System.out.println(bit);        
+    }
+    
+}
