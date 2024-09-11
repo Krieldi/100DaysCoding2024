2d411b8514e3ea3677a64f962cc8f2e38cd3e074
Krieldi Mesa Karaeng <kejesks31@gmail.com>
2024-09-12 07:05:29 +0800

    Hari ke Sebelas

---
+++ Day11.java
@@ -0,0 +1,12 @@
+package ISC;
+
+public class Day11 {
+    
+    public static void main(String[] args) {
+        short nmbr= 32767;//Short mencakup anka -32.768 sampai 32.767
+        short no= -32768;//32 bit
+        System.out.println(nmbr);        
+        System.out.println(no);        
+    }
+    
+}
