b49c8f930001c3c2ef495e479ff93957287dfea0
Krieldi Mesa Karaeng <kejesks31@gmail.com>
2024-09-13 10:55:41 +0800

    Hari ke dua belas

---
+++ Day12.java
@@ -0,0 +1,24 @@
+package ISC;
+
+public class Day12 {
+    
+    public static void main(String[] args) {
+        //2 contoh penggunaan tipe data integer
+        int value = 123456789;//int hanya mencakup 9 digit angka
+        
+        int a= 4;
+        int b= 4;
+        
+        int plus= a + b;
+        int min= a - b;
+        int x= a * b;
+        int bagi= a / b;
+        
+        System.out.println("cakupan digit: "+value);
+        System.out.println("hasil penjumlahan: "+plus);
+        System.out.println("hasil pengurangan: "+min);
+        System.out.println("hasil perkalian: "+x);
+        System.out.println("hasil pembagian: "+bagi);
+    }
+    
+}
