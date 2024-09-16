99c7bb3b4385773f36e94f4a8ccfdfc6050c8a7c
Krieldi Mesa Karaeng <kejesks31@gmail.com>
2024-09-16 08:34:54 +0800

    Hari ke lima belas

---
+++ Day15.java
@@ -0,0 +1,18 @@
+package ISC;
+
+public class Day15 {
+    
+    public static void main(String[] args) {
+        
+        //double untuk angka desimal
+        double value= 3.14159D;//tidak di haruskan memakai huruf 'd' namun d hanya untuk penanda.
+        
+        double a= 4.4;
+        double b= 2.2;
+        double c=a+b;//hasilnya akan menjadi 16 digit di belakang koma
+        
+        System.out.println("contoh double:"+value);
+        System.out.println("Hasil: "+c);
+        
+    }
+}
