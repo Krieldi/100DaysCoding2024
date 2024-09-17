cd886416e469ee6abc6d33c142be931053917b8f
Krieldi Mesa Karaeng <kejesks31@gmail.com>
2024-09-17 20:00:02 +0800

    Hari ke enam belas

---
+++ Day16.java
@@ -0,0 +1,17 @@
+package ISC;
+
+public class Day16 {
+    
+    public static void main(String[] args) {
+        //contoh penggunaan tipe data float
+        float value= 3.14159F;// harus menggunakan tanda 'f' karena java akan menganggapnya double
+        //contoh penjumlahan float
+        float a = 4.4F;
+        float b = 2.2F;
+        float c = a+b;//penjumlahan
+        
+        //print:
+        System.out.println("contoh float: "+value);
+        System.out.println("Hasil: "+c);
+    }
+}
