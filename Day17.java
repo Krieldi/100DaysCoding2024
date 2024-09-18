7afd40e710640a0805259ed83610fdff192cdec5
Krieldi Mesa Karaeng <kejesks31@gmail.com>
2024-09-18 21:07:48 +0800

    Hari ke tujuh belas

---
+++ Day17.java
@@ -0,0 +1,17 @@
+package ISC;
+
+public class Day17 {
+    
+    public static void main(String[] args) {
+        //contoh penggunaan tipe data char
+        char i= 'A';
+        
+        //sayapun bingung tentang unicode hehe
+        //contoh unicode di java yang terdapat dalam tipe data char:
+        char unicode= 65;//outputnya akan menjadi 'A', karena 65 adalah kode unik dari 'A'
+             
+        
+        System.out.println("Contoh character: "+i);
+        System.out.println("Contoh unicode: "+unicode;
+    }
+}
