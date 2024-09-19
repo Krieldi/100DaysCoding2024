ae23d69fc78497ad89292a253482d8f2c404cfce
Krieldi Mesa Karaeng <kejesks31@gmail.com>
2024-09-19 18:41:13 +0800

    Hari ke depan belas

---
+++ Day18.java
@@ -0,0 +1,21 @@
+package ISC;
+
+public class Day18 {
+    
+    public static void main(String[] args) {
+        //cara penggunaan boolean
+        boolean status= false;
+        //contoh operasi logika not di boolean
+        boolean hasil= true;
+        boolean hasilnot= !hasil;
+        
+        //operator perbandingan di Boolean
+        int a= 10;
+        int b = 20;
+        boolean result = (a < b);
+        
+        System.out.println("contoh: "+status);
+        System.out.println("Contoh logika Not: "+hasilnot);
+        System.out.println("Apakah a lebih kecil dari b: "+result);
+    }    
+}
