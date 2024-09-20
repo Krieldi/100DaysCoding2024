6a30bc15b87e7b6947dc427eeeb1b6e1ee793d47
Krieldi Mesa Karaeng <kejesks31@gmail.com>
2024-09-20 19:13:14 +0800

    Hari ke sembilan belas

---
+++ Day19.java
@@ -0,0 +1,15 @@
+package ISC;
+
+public class Day19 {
+    
+    public static void main(String[] args) {
+        
+        //inisialisasi String
+        String name= "Krieldi";
+        String Fname= "Krieldi "+"Mesa "+"Karaeng";
+        
+        System.out.println("Name: "+name);
+        System.out.println("Full Name: "+ Fname);
+        
+    }
+}
