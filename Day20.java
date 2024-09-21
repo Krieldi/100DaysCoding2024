1053da2fbaff1204967b1b93e1cbbcc7b815fb65
Krieldi Mesa Karaeng <kejesks31@gmail.com>
2024-09-21 21:08:03 +0800

    Hari ke dua puluh

---
+++ Day20.java
@@ -0,0 +1,16 @@
+package ISC;
+
+public class Day20 {
+    
+    public static void main(String[] args) {
+        //Byte adalah tipe data object dan berbeda dengan byte yang hanya dapat menyimpan nilai
+        Byte cnth= 123;
+        System.out.println("Contoh penggunaan: "+cnth);
+        
+        // Contoh constanta untuk mendapatkan nilai maksimal dari Byte
+        Byte max= Byte.MAX_VALUE;
+        Byte min= Byte.MIN_VALUE;
+        System.out.println("Max Value: " + max);
+        System.out.println("Min Value: " +min);
+    }
+}
