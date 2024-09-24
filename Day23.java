236ef1c33263bce20e738b1f543ab7276fc0c9bd
Krieldi Mesa Karaeng <kejesks31@gmail.com>
2024-09-24 13:09:45 +0800

    Hari ke dua puluh tiga

---
+++ Day23.java
@@ -0,0 +1,15 @@
+package ISC;
+
+public class Day23 {
+    
+    public static void main(String[] args) {
+        long long1 = 200000L;
+        Long long2 = Long.valueOf(long1);
+        Long max = Long.MAX_VALUE;
+        Long min = Long.MIN_VALUE;
+        
+        System.out.println("Contoh penggunaan: "+long2);
+        System.out.println("Max value long: "+max);
+        System.out.println("Min value long: "+min);
+    }
+}
