041861cb80bfb9459067e05aba9ae4360f3f8c2c
Krieldi Mesa Karaeng <kejesks31@gmail.com>
2024-09-27 20:58:20 +0800

    Hari ke dua puluh enam

---
+++ Day26.java
@@ -0,0 +1,10 @@
+package ISC;
+public class Day26{
+   public static void main (String[]args){
+       Character ch= 'A';//Auto-boxing
+       
+       System.out.print("Contoh cara penggunaan tipe data Character adalah: ");
+       System.out.println("Character ...= 'A';");
+       System.out.println("Hasil unicode: "+(int)ch);
+   } 
+}
