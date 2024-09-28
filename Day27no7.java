525f63d3c27548eaa765c4948c28924c4789562d
Krieldi Mesa Karaeng <kejesks31@gmail.com>
2024-09-28 19:59:01 +0800

    Evaluasi Mentor hari ke dua puluh tujuh

---
+++ Day27no7.java
@@ -0,0 +1,12 @@
+package ISC;
+public class Day27no7 {
+    public static void main(String[] args) {
+        System.out.println("Selamat datang di catatan jadwal kegiatan Sekolah Cerdas Mandiri!");
+        Integer jumlahKegiatan= 10;
+        final String namaKegiatan="Perayaan Hari Kemerdekaan";//Variable constanta
+        float durasiKegiatan= 3.5f;
+        durasiKegiatan= 4.5f;
+        System.out.printf("Kegiatan %s memiliki durasi %.1fjam. Jumlah kegiatan yanh direncanakan saat ini adalah %d",namaKegiatan,durasiKegiatan,jumlahKegiatan);
+        
+    }
+}
