58c43458c0944d09ea4a5aff1ddb13b45c2d6afe
Krieldi Mesa Karaeng <kejesks31@gmail.com>
2024-09-29 22:33:08 +0800

    Hari ke dua puluh delapan

---
+++ Day28.java
@@ -0,0 +1,14 @@
+package ISC;
+
+public class Day28 {
+    public static void main(String[] args) {
+        //nilai boolean
+        Boolean a= true;
+        Boolean b= false;        
+        Boolean c= a==b;
+        
+        System.out.println("Nilai a= "+a);
+        System.out.println("Nilai b= "+b);        
+        System.out.println("apakah nilai a dan b sama? "+c);
+    }
+}
