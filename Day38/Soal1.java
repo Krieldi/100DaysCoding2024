38a94ef883df173fbea413512ed67083ff218820
Krieldi Mesa Karaeng <kejesks31@gmail.com>
2024-10-09 16:03:05 +0800

    Soal pertama

--- Day38/Soal1.java
+++ Day38/Soal1.java
@@ -1,1 +1,16 @@
+package ISC;
 
+import java.util.Scanner;
+
+public class Day38 {
+    
+    public static void main(String[] args) {
+        //soal pengisian dan perkalian
+        Scanner in=new Scanner (System.in);
+        System.out.print("Masukkan Luas lahan(ha): ");
+        int a= in.nextInt();
+        a *= 2;
+        
+        System.out.println("Setelah setahun luas lahan yang di punya setelah digandakan adalah "+a+" Hektare(ha)");
+    }
+}
