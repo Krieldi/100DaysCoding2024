932dc16e70ec3d11024d6d22e64a6eedaf07f683
Krieldi Mesa Karaeng <kejesks31@gmail.com>
2024-10-14 22:03:33 +0800

    Hari ke empat puluh tiga

---
+++ Day43/Day43_2.java
@@ -0,0 +1,14 @@
+package ISC;
+import java.util.Scanner;
+public class Day43_2 {
+    public static void main(String[] args) {
+        Scanner in= new Scanner(System.in);
+        System.out.print("Masukkan nilai: ");
+        int q= in.nextInt();
+        System.out.print("Masukkan nilai: ");
+        int e= in.nextInt();
+        
+        boolean a= q<=e;
+        System.out.printf("Jika nilai %d <= %d maka %b ",q,e,a);
+    }
+}
