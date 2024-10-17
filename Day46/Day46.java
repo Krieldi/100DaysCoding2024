498dc96cd72fe68d6491de1cb496f935a16608eb
Krieldi Mesa Karaeng <kejesks31@gmail.com>
2024-10-17 22:41:13 +0800

    Hari ke empat puluh enam

---
+++ Day46/Day46.java
@@ -0,0 +1,14 @@
+package ISC;
+public class Day46 {
+    public static void main(String[] args) {
+        boolean a= false;
+        a = !a;
+        System.out.println("Hasil: "+a);
+        
+        int satu= 4;
+        int dua= 7;
+        boolean b= satu > dua;
+        System.out.println("Hasil: "+b);
+                
+    }
+}
