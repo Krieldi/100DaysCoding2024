0d4fe36c6e297fa6239eceaab8f54fd653d2785f
Krieldi Mesa Karaeng <kejesks31@gmail.com>
2024-10-18 20:56:21 +0800

    Hari ke empat puluh tujuh

---
+++ Day47/Day47.java
@@ -0,0 +1,15 @@
+package ISC;
+import java.util.Scanner;
+public class Day47 {
+    
+    public static void main(String[] args) {
+        Scanner in= new Scanner (System.in);
+        System.out.print("Masukkan nama yang sesuai: ");
+        String a= in.nextLine();
+        
+        if (a.equalsIgnoreCase("Krieldi")){
+            System.out.println("Nama : Krieldi");
+            System.out.println("NIM  : D0224340");
+        }        
+    }
+}
