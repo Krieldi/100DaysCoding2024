56a674f48aa503559419624a3b5262c890afc6fe
Krieldi Mesa Karaeng <kejesks31@gmail.com>
2024-10-26 20:24:11 +0800

    Hari ke lima puluh lima

---
+++ Day55/Day55.java
@@ -0,0 +1,9 @@
+package ISC;
+
+public class Day55 {
+    public static void main(String[] args) {
+        for (int i = 10; i > 0; i--) {
+            System.out.println("Perulangan decrement ke-"+i);
+        }
+    }
+}
