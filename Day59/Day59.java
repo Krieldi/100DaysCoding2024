815218cec3acc6d5c4baa1e1d716d3286fb2d8a6
Krieldi Mesa Karaeng <kejesks31@gmail.com>
2024-10-30 18:34:10 +0800

    Hari ke lima puluh sembilan

---
+++ Day59/Day59.java
@@ -0,0 +1,10 @@
+package ISC;
+public class Day59 {
+    public static void main(String[] args) {
+        int[] a={1, 2, 3, 4, 5};
+        System.out.print("Bilangan dalam array: ");
+        for (int i: a){
+            System.out.print(i+" ");
+        }
+    }
+}
