ba15ca4043ffee95a78b5fc3e856637586e142aa
Krieldi Mesa Karaeng <kejesks31@gmail.com>
2024-10-31 17:08:57 +0800

    Hari ke enam puluh

---
+++ Day60/Day60.java
@@ -0,0 +1,11 @@
+package ISC;
+
+public class Day60 {
+    public static void main(String[] args) {
+        int c = 0;
+        do {
+            System.out.println(c);
+            c++;
+        } while (c <= 0);
+    }
+}
