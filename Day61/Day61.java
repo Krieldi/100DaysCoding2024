7135870a01e237bfb3544cb1704c44e76c7deaf3
Krieldi Mesa Karaeng <kejesks31@gmail.com>
2024-11-01 21:53:16 +0800

    Hari ke enam puluh satu

---
+++ Day61/Day61.java
@@ -0,0 +1,16 @@
+package ISC;
+
+public class Day61 {
+    
+    public static void main(String[] args) {
+        // infinite loop dengan while
+        /* while(true){
+            int a= 1;
+            System.out.println("Inf-Loop: "+a);
+        }*/
+        // infinite loop dengan for
+        for(;;){
+            System.out.println("infinite loop");
+        }
+    }
+}
