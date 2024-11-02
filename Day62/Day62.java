d281a06b59af05528f7acd0160796080c5158af7
Krieldi Mesa Karaeng <kejesks31@gmail.com>
2024-11-02 16:10:41 +0800

    Hari ke enam puluh dua

---
+++ Day62/Day62.java
@@ -0,0 +1,14 @@
+package ISC;
+
+public class Day62 {
+
+    public static void main(String[] args) {
+        for (int x = 1; x <= 10; x++) {
+            if (x == 5) {
+                System.out.println("Angka 5, BREAK!!");
+                break; // Keluar(break) dari loop saat x = 5
+            }
+            System.out.println("Nilai x: " + x);
+        }
+    }
+}
