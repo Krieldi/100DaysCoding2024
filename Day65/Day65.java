05a1709201d0594c4d5dc60bc4e44de778bc9b95
Krieldi Mesa Karaeng <kejesks31@gmail.com>
2024-11-05 23:54:25 +0800

    Hari ke enam puluh lima

---
+++ Day65/Day65.java
@@ -0,0 +1,15 @@
+package ISC;
+
+public class Day65 {
+
+    public static void main(String[] args) {
+        // DAY65 el(break)
+        int i = 1;
+        for (; i <= 4; i++) {
+            for (int j = 1; j <= 4; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
