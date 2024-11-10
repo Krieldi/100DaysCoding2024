959c4d98250a8a602a41925803186754004de33d
Krieldi Mesa Karaeng <kejesks31@gmail.com>
2024-11-10 20:16:26 +0800

    Hari ke tujuh puluh

---
+++ Day70/Day70.java
@@ -0,0 +1,11 @@
+package ISC;
+
+public class Day70 {
+    public static void main(String[] args) {
+        Day70 Kelazz= new Day70();
+        Kelazz.nilai(5, 5);
+    }
+    void nilai (int a, int b){
+        System.out.println(a+b);
+    }
+}
