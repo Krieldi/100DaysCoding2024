0b7a113d3fc9ba2e34c4b268f5749a56192104a4
Krieldi Mesa Karaeng <kejesks31@gmail.com>
2024-11-16 22:50:42 +0800

    Hari ke tujuh puluh enam

---
+++ Day76/Day76.java
@@ -0,0 +1,9 @@
+package ISC;
+public class percobaan {
+    public static void main(String[] args) {
+        String a= "hello world!";
+        System.out.println("kata awal: "+a);
+        String b= a.toUpperCase();
+        System.out.println("Setelah di ubah: "b);
+    }
+}
