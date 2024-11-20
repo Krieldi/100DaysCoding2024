0e9bf805439e44282864793f851af183d6cc1936
Krieldi Mesa Karaeng <kejesks31@gmail.com>
2024-11-20 20:03:24 +0800

    Hari ke delapan puluh

---
+++ Day80/Day80.java
@@ -0,0 +1,10 @@
+package ISC;
+
+public class Day80 {
+    public static void main(String[] args) {
+        String a= " Krieldi ";
+        String t= a.trim();
+        System.out.println("'"+a+"'");
+        System.out.println("'"+t+"'");
+    }
+}
