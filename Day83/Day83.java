123f2602e3bbe1ea4b7ae8fdbf2c46d062766aac
Krieldi Mesa Karaeng <kejesks31@gmail.com>
2024-11-24 05:24:37 +0800

    Hari ke delapan puluh tiga

---
+++ Day83/Day83.java
@@ -0,0 +1,9 @@
+package ISC;
+
+public class Day83 {
+
+    public static void main(String[] args) {
+        char[] a={'K','R','I','E','L','D','I'};
+        System.out.println("Panjang dari nama 'Krieldi'"+" adalah: "+a.length);
+    }
+}
