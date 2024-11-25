16c29d1e18486ddde86cfb2314c1860a2a3344eb
Krieldi Mesa Karaeng <kejesks31@gmail.com>
2024-11-25 20:00:00 +0800

    Hari ke delapan puluh lima

---
+++ Day85/Day85.java
@@ -0,0 +1,13 @@
+package ISC;
+
+import java.util.*;
+
+public class Day85 {
+
+    public static void main(String[] args) {
+        // Hari ke delapan puluh lima
+        // Mengisi elemen ke Array satu dimensi pada saat di inisialisasi
+        int[] a = {1, 2, 3, 4, 5};
+        System.out.println("Array: " + Arrays.toString(a));
+    }
+}
