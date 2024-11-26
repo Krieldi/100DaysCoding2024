f5e6038ff8e007652fd35ffec57275bb32163e80
Krieldi Mesa Karaeng <kejesks31@gmail.com>
2024-11-26 11:29:07 +0800

    Hari ke delapan puluh enam

---
+++ Day86/Day86.java
@@ -0,0 +1,13 @@
+package ISC;
+
+public class Day86 {
+
+    public static void main(String[] args) {
+        // Mencetak elemen pada Array satu dimensi
+        int[] a = {1, 2, 3, 4, 5};
+        System.out.println("Elemen array: ");
+        for (int i = 0; i < a.length; i++) {
+            System.out.println("Index " + i + " = " + a[i]);
+        }
+    }
+}
