138d569743266fc5313fe231f3f9176e55369a06
Krieldi Mesa Karaeng <kejesks31@gmail.com>
2024-11-30 20:39:03 +0800

    Hari ke sembilan puluh

---
+++ Day90/Soal3.java
@@ -0,0 +1,9 @@
+package Day90;
+
+public class Soal3 {
+    public static void main(String[] args) {
+        for (int i = 50; i >=5; i-=5) {
+            System.out.print(i+" ");
+        }
+    }
+}
