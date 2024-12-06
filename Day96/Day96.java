d057c893b0809816b4152d39b3c40be5dd0edc93
Krieldi Mesa Karaeng <kejesks31@gmail.com>
2024-12-06 21:59:30 +0800

    Hari ke sembilan puluh enam

---
+++ Day96/Day96.java
@@ -0,0 +1,34 @@
+package ISC;
+
+import java.util.Scanner;
+
+public class Day96 {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        for (int i = 1; i <= n; i++) {
+            for (int j = n; j > i; j--) {
+                System.out.print(" ");
+            }
+            for (int k = 1; k <= 2 * i - 1; k++) {
+                if (i >= n-2) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+        for (int i = n - 1; i >= 1; i--) {
+            for (int j = n; j > i; j--) {
+                System.out.print(" ");
+            }
+            for (int k = 1; k <= (2 * i - 1); k++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        in.close();
+    }
+}
